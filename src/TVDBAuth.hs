22f87594dcafb881321183d19b6db91b497fa2c0
Saul Glasman <saulglasman0@gmail.com>
2020-02-21 07:40:00 -0500

    WIP

--- src/TVDBAuth.hs
+++ src/TVDBAuth.hs
@@ -20,7 +20,7 @@ import App
 import Control.Monad.Error.Class (catchError)
 import Control.Monad.State.Class (get, put)
 
-runAuthenticated :: (MonadIO m, HttpMethod method, ToJSON a, FromJSON b, HttpBodyAllowed (AllowsBody method) 'CanHaveBody) => Request a b method -> App m b
+runAuthenticated :: Requests.RequestOK a b m method => Request a b method -> App m b
 runAuthenticated req = do
   token <- get
   let res = makeRequest req token
